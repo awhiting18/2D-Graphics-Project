88030f827de2b904367d8171a89cb71b24d376e2
AJ Whiting <55210347+awhiting18@users.noreply.github.com>
2021-09-08 12:57:42 -0500

    Updated the code and now it is done with tutorial 4. There is a bug of an array out of bounds but I will figure that out later

--- main/Main.java
+++ main/Main.java
@@ -7,26 +7,27 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
-		double[][] weights = GaussianBlur.getInstance().generateWeightMatrix(150, Math.sqrt(150));
+
+		double[][] weights = GaussianBlur.getInstance().generateWeightMatrix(150, 1.5);
 		GaussianBlur.getInstance().printWeightedMatrixToFile(weights);
-		
+
 		BufferedImage answer = null;
 		try {
-			BufferedImage source_image = ImageIO.read(new File("C:/Users/AJ/Desktop/School/2DGraphics_ImageProc/2DGraphics_Project/2D-Graphics-Project/bg_3.jpeg"));
+			String filePath = "./giraffe.jpeg";
+			BufferedImage source_image = ImageIO.read(new File(filePath));
 			answer = GaussianBlur.getInstance().createGaussianedImage(source_image, weights, 20);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
 			ImageIO.write(answer, "PNG", new File("answer.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Done!");
 	}
 
