4086e9cca10b7e340bd847a06b0513cfa2ba3dcc
AJ Whiting <55210347+awhiting18@users.noreply.github.com>
2021-10-13 12:07:50 -0500

    changed my python code

--- Blurs.py
+++ Blurs.py
@@ -1,13 +1,59 @@
 from PIL import Image, ImageDraw
 
-# Load image:
-input_image = Image.open("oldCar.jpg")
-input_pixels = input_image.load()
 
-# Box Blur kernel
-box_kernel = [[1 / 9, 1 / 9, 1 / 9],
-              [1 / 9, 1 / 9, 1 / 9],
-              [1 / 9, 1 / 9, 1 / 9]]
+def blur(image, kernel):
+    # Load image:
+    input_image = Image.open(image)
+    input_pixels = input_image.load()
+
+    # Middle of the kernel
+    offset = len(kernel) // 2
+
+    # Create output image
+    output_image = Image.new("RGB", input_image.size)
+    draw = ImageDraw.Draw(output_image)
+
+    # Compute convolution between intensity and kernels
+    for x in range(offset, input_image.width - offset):
+        for y in range(offset, input_image.height - offset):
+            acc = [0, 0, 0]
+            for a in range(len(kernel)):
+                for b in range(len(kernel)):
+                    xn = x + a - offset
+                    yn = y + b - offset
+                    pixel = input_pixels[xn, yn]
+                    acc[0] += pixel[0] * kernel[a][b]
+                    acc[1] += pixel[1] * kernel[a][b]
+                    acc[2] += pixel[2] * kernel[a][b]
+
+            draw.point((x, y), (int(acc[0]), int(acc[1]), int(acc[2])))
+
+    output_image.save("output.jpg")
+
+
+def blurIteration(inputImage, outputImage, kernal, iterations):
+    print("Working on iteration 1")
+    blur(inputImage, kernal)
+    print("Iteration 1 done")
+    for x in range(1, iterations):
+        print("Working on iteration ", x + 1)
+        blur(outputImage, kernal)
+        print("Iteration ", x + 1, " done")
+
+        # Box Blur kernel
+box_kernel3x3 = [[1 / 9, 1 / 9, 1 / 9],
+                 [1 / 9, 1 / 9, 1 / 9],
+                 [1 / 9, 1 / 9, 1 / 9]]
+
+box_kernel9x9 = [[1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81],
+                 [1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81],
+                 [1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81],
+                 [1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81],
+                 [1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81],
+                 [1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81],
+                 [1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81],
+                 [1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81],
+                 [1 / 81, 1 / 81, 1 / 81, 1/81, 1/81, 1/81, 1/81, 1/81, 1/81], ]
 
 # Gaussian kernel
 gaussian_kernel = [[1 / 256, 4 / 256,  6 / 256,  4 / 256, 1 / 256],
@@ -17,28 +63,6 @@ gaussian_kernel = [[1 / 256, 4 / 256,  6 / 256,  4 / 256, 1 / 256],
                    [1 / 256, 4 / 256,  6 / 256,  4 / 256, 1 / 256]]
 
 # Select kernel here:
-kernel = gaussian_kernel
+kernel = box_kernel3x3
 
-# Middle of the kernel
-offset = len(kernel) // 2
-
-# Create output image
-output_image = Image.new("RGB", input_image.size)
-draw = ImageDraw.Draw(output_image)
-
-# Compute convolution between intensity and kernels
-for x in range(offset, input_image.width - offset):
-    for y in range(offset, input_image.height - offset):
-        acc = [0, 0, 0]
-        for a in range(len(kernel)):
-            for b in range(len(kernel)):
-                xn = x + a - offset
-                yn = y + b - offset
-                pixel = input_pixels[xn, yn]
-                acc[0] += pixel[0] * kernel[a][b]
-                acc[1] += pixel[1] * kernel[a][b]
-                acc[2] += pixel[2] * kernel[a][b]
-
-        draw.point((x, y), (int(acc[0]), int(acc[1]), int(acc[2])))
-
-output_image.save("output.png")
+blurIteration("oldCar.jpg", "output.jpg", kernel, 3)
